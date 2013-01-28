098ec6974ce9f084d976d32b9b0eaa2078888ef9
Yongnam Park <devnoff@gmail.com>
2013-01-28 19:50:45 +0900

    라이크한 상품 보기 컨트롤러 추가

---
+++ caseshop/Classes/Controllers/Shop/ProductLikedController.h
@@ -0,0 +1,13 @@
+//
+//  ProductLikedController.h
+//  caseshop
+//
+//  Created by Yongnam Park on 13. 1. 28..
+//  Copyright (c) 2013년 CultStory Inc. All rights reserved.
+//
+
+#import "ProductListController.h"
+
+@interface ProductLikedController : ProductListController
+
+@end
---
+++ caseshop/Classes/Controllers/Shop/ProductLikedController.m
@@ -0,0 +1,13 @@
+//
+//  ProductLikedController.m
+//  caseshop
+//
+//  Created by Yongnam Park on 13. 1. 28..
+//  Copyright (c) 2013년 CultStory Inc. All rights reserved.
+//
+
+#import "ProductLikedController.h"
+
+@implementation ProductLikedController
+
+@end
